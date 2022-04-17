ac8efef6645caa7d113eb004b5c17465f53044fc
Poonamjnvu <Poonamjnvu>
2022-04-17 19:31:05 +0530

    save file

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -2,4 +2,10 @@ console.log("working");
 
 
 const a = document.getElementsByClassName("box")
-console.log(a);
+console.log(a[0].innerText);
+
+// function changeText(){
+//     const try1 = document.getElementsByClassName("box");
+//     try1[0].innerText = "Object in java script";
+//     console.log(try1);
+// }
