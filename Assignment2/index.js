3313bc3dae71438f303200f037214c8a69861cb9
Poonamjnvu <Poonamjnvu>
2022-04-17 19:19:52 +0530

    Add file

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -1,2 +1,7 @@
-const a = document.getElementsByTagName(h1);
-console.log(h1);
+console.log("working");
+
+
+const a = document.getElementsByTagName("h1");
+console.log(a[0].innerText);
+  
+a[0].innerText = "welcome to javascript";
